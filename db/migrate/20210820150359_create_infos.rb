266ef103855cfe8477526ad8843d014ba2937805
JSONRete <jsonrete@gmail.com>
2021-08-20 11:18:44 -0400

    changed t.data type from numeric to numbers

--- db/migrate/20210820150359_create_infos.rb
+++ db/migrate/20210820150359_create_infos.rb
@@ -2,7 +2,7 @@ class CreateInfos < ActiveRecord::Migration[6.1]
   def change
     create_table :infos do |t|
       t.string :name
-      t.numbers :tempo
+      t.numeric :tempo
       t.string :key
       t.belongs_to :tool, null: false, foreign_key: true
 
