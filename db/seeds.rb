31c6c6faf7441b60f1b5805cabb25a333a21cd65
JSONRete <jsonrete@gmail.com>
2021-08-21 07:54:02 -0400

    working on seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+Info.destroy_all
+Tool.destroy_all
+
+infos = 12.times.map do
+    Info.create(key: Faker::Music.key)
+  end
+
+
+
+mpc = Tool.create(name: 'MPC3000')
+sp = Tool.create(name: 'SP-1200')
+pt = Tool.create(name: 'Pro Tools®')
+lx = Tool.create(name: 'Logic Pro X')
+303 = Tool.create(name: 'SP-303')
+404 = Tool.create(name: 'SP-404')
+isla = Tool.create(name: 'S2400')
+ni = Tool.create(name: 'MASCHINE')
+al = Tool.create(name: 'Ableton Live')
+
+Info.create([
+    {name: 'Pop Heart', tempo: 120,  key: info.infos, tool: mpc},
+
+])
