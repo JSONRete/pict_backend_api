46f00a1c11eb697bda425901f954bbf8508c65f9
JSONRete <jsonrete@gmail.com>
2021-08-24 18:39:43 -0400

    commented out tool_controller crud actions

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,54 +1,54 @@
-class ToolsController < ApplicationController
-  before_action :set_tool, only: [:show, :update, :destroy]
+# class ToolsController < ApplicationController
+#   before_action :set_tool, only: [:show, :update, :destroy]
 
-  # GET /tools
-  def index
-    tools = Tool.all
-    render json: ToolSerializer.new(tools)
+#   # GET /tools
+#   def index
+#     tools = Tool.all
+#     render json: ToolSerializer.new(tools)
 
 
 
-    # render json: tools
-  end
+#     # render json: tools
+#   end
 
-  # GET /tools/1
-  def show
-    render json: tool
-  end
+#   # GET /tools/1
+#   def show
+#     render json: tool
+#   end
 
-  # POST /tools
-  def create
-    tool = Tool.new(tool_params)
+#   # POST /tools
+#   def create
+#     tool = Tool.new(tool_params)
 
-    if tool.save
-      render json: tool, status: :created, location: tool
-    else
-      render json: tool.errors, status: :unprocessable_entity
-    end
-  end
+#     if tool.save
+#       render json: tool, status: :created, location: tool
+#     else
+#       render json: tool.errors, status: :unprocessable_entity
+#     end
+#   end
 
-  # PATCH/PUT /tools/1
-  def update
-    if tool.update(tool_params)
-      render json: tool
-    else
-      render json: tool.errors, status: :unprocessable_entity
-    end
-  end
+#   # PATCH/PUT /tools/1
+#   def update
+#     if tool.update(tool_params)
+#       render json: tool
+#     else
+#       render json: tool.errors, status: :unprocessable_entity
+#     end
+#   end
 
-  # DELETE /tools/1
-  def destroy
-    tool.destroy
-  end
+#   # DELETE /tools/1
+#   def destroy
+#     tool.destroy
+#   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tool
-      tool = Tool.find(params[:id])
-    end
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_tool
+#       tool = Tool.find(params[:id])
+#     end
 
-    # Only allow a list of trusted parameters through.
-    def tool_params
-      params.require(:tool).permit(:name)
-    end
-end
+#     # Only allow a list of trusted parameters through.
+#     def tool_params
+#       params.require(:tool).permit(:name)
+#     end
+# end
